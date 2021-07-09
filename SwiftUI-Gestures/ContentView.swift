5e357baadc631e1da113851cd5ce3bf3b030d0ca
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-09 16:41:02 +0530

    Initial Commit

---
+++ SwiftUI-Gestures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Gestures
+//
+//  Created by Arpit Dixit on 09/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Gestures/SwiftUI_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GesturesApp.swift
+//  SwiftUI-Gestures
+//
+//  Created by Arpit Dixit on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
